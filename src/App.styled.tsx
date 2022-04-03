129c5fd5ef5f7d1b321155c1019d41d0ec8ebf74
Kos Krzysztof <krzysztof.kos33@gmail.com>
2022-04-03 20:46:57 +0200

    Huskt test-9

--- src/App.styled.tsx
+++ src/App.styled.tsx
@@ -1,14 +1,5 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  color: teal;
-
-  &::before {
-    content: 'aaa';
-    dsiplay: block;
-  }
-
-  &:hover {
-    color: red;
-  }
+  color: #232323;
 `;
