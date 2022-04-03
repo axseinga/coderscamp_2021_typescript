9c181c8e5e56b6ae1cd7e6c0c008a683382061cf
Kos Krzysztof <krzysztof.kos33@gmail.com>
2022-04-03 19:28:48 +0200

    Husky test

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root') as HTMLDivElement;
+const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
